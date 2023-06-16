22cf2c5468c80f90d078d44048bebf5c85adcb05
marcosfuentes94 <marcoslionel@hotmail.it>
2023-06-16 17:51:16 +0200

    avanzamento milestone 3

---
+++ script.js
@@ -0,0 +1,19 @@
+// SELEZIONA IL CONTENITORE DEL CAROUSEL
+const carouselContainer = document.querySelector('.carousel-container');
+// SELEZIONA TUTTE LE SLIDE DEL CAROUSEL
+const carouselItems = document.querySelectorAll('.carousel-item');
+// INDICE SLIDE
+let currentIndex = 0;
+// AGGIORNA L'INDICE CORRENTE IN BASE ALLA DIREZIONE DI SCORRIMENTO
+function changeSlide(direction) {
+  currentIndex += direction;
+// SE L'INDICE E' NEGATIVO TORNA ALL'ULTIMA SLIDE
+  if (currentIndex < 0) {
+    currentIndex = carouselItems.length - 1;
+// SE L'INDICE SUPERA IL NUMERO DI SLIDE, TORNA ALLA PRIMA SLIDE    
+  } else if (currentIndex >= carouselItems.length) {
+    currentIndex = 0;
+  }
+// SPOSTA IL CONTENITORE DEL CAROUSEL PER VISUALIZZARE LE SLIDE ORIZZONTALMENTE
+  carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`; 
+}
